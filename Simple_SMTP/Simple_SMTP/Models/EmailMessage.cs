9a729f5dd7d26fe5859afe573b4d0cdbb93d0327
Jaiden Chicote <jaiden_chicote@outlook.com>
2025-03-02 16:31:34 +1100

    Add EmailMessage model

---
+++ Simple_SMTP/Simple_SMTP/Models/EmailMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_SMTP.Models
+{
+
+    public class EmailMessage
+    {
+
+        #region - - - - - Properties - - - - -
+
+        public DateTime SentTime { get; set; } = DateTime.Now;
+
+        public string Body { get; set; }
+
+        public string Subject { get; set; }
+
+        public string To { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
